698d81bd06ec402780ebc84e38a4c504947131d6
mark91 <mark.mekhayl@gmail.com>
2023-11-13 17:49:36 +1100

    almost done

---
+++ src/components/Portfolio.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Some of my projects so far, you can check them out in more depth on my <a href="https://github.com/Lozlink">GitHub</a></h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <a href={item.link} target="_blank" rel="noreferrer">
+                  <div className="item-wrap">                     
+                        <img src={`${item.imgurl}`} className="item-img" alt='portfolio item'/>               
+                  </div>
+                  </a>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Mark Mekhayl",
+    "role": "Junior Web Developer || Instructional Associate",
+    "linkedinId":"mark-mekhayl",
+    "roleDescription": "I'm a Junior Web Developer with a background in sales within various industries. On the hunt for a junior role in Sydney with a preference for JavaScript and React! Currently working as an Instructional Associate",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mark-mekhayl/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Lozlink",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"Hey, Mark here! A Junior Developer with 6 years of experience in diverse sales roles within various industries. After completing and subsequently becoming an Instuctor Associate in a full stack software development course, I am now seeking a role where I can add value to a business through applying my passion for technology and leveraging my past experience in sales. My passion for technology drives me to find innovative solutions to problems, while my sales background gives me valuable insights into customer needs and market trends.",
+    "address":"Sydney, Australia",
+    "website":<a href="https://github.com/Lozlink">GitHub</a>,
+    "education":[
+      {
+        "UniversityName":"Western Sydney University",
+        "specialization":"Marketing & Brand Management",
+        "MonthOfPassing":"",
+        "YearOfPassing":":   2013 - 2017",
+      },
+      {
+        "UniversityName":"General Assembly Software Immersive",
+        "specialization":"",
+        "MonthOfPassing":"",
+        "YearOfPassing":"November 2022 - February 2023",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"General Assembly",
+        "specialization":"Instructional Associate",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":" March 2023 - Present",
+      },
+      {
+        "CompanyName":"Sunshade Shutters & Blinds",
+        "specialization":"Sales & Marketing Manager",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"February 2021 - May 2022",
+      },
+      {
+        "CompanyName":"Alsco Australia",
+        "specialization":"Sales Executive",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"November 2019 - February 2021",
+      },
+      {
+        "CompanyName":"Winc Australia",
+        "specialization":"Business Development Consultant",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"April 2017 - July 2019",
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Ruby"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"HTML"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Budgiet",
+        "description":"Budgiet is a simple Budgeting tool that takes user authentication into effect and allows end users to manage their finances. Utilizing React.js for the front end, node.js for the backend, and PSQL as a persisting database to store users and their accounts.",
+        "imgurl":"images/Budgiet.JPG",
+        "link": "https://github.com/Lozlink/Budgiet"
+      },
+      {
+        "name":"Sprite Knite",
+        "description":"Sprite Knight is an interactive game in which users can create, fight and level up their own characters. Users have the ability to create multiple characters. Note: This project was created in collaboration with 3 others.",
+        "imgurl":"images/SpriteKnightHomePage.png",
+        "link": "https://github.com/Lozlink/sprite-knight"
+      },
+      {
+        "name":"Hyrule's Tic Tac Toe",
+        "description":"Tic Tac Toe based on an IP that I have been fond of for over 25 years. Play as either Link or Zelda from the The Legend of Zelda Franchise",  
+        "imgurl":"images/Port TTT photo.JPG",
+        "link": "https://lozlink.github.io/Link-s-TTT/"
+      },
+      {
+        "name":"Mark's Gaming Database",
+        "description":"Mark's Gaming Database is a CRUD application in which users can navigate games, as well as utilizing user authentication to add games to their wishlist upon logging in.",
+        "imgurl":"https://i.imgur.com/w2ndXo2l.jpg",
+        "link": "https://github.com/Lozlink/Game-Rental-Store-"
+      }
+    ],
+    
+  }
+  
+  export default resumeData
